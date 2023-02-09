016fa1c4633388bb51fdd3507ffcac81bba39fdc
danieljpender <danielpp4321@hotmail.co.uk>
2023-02-09 22:22:06 +0000

    Update MySchedule.php

--- MySchedule.php
+++ MySchedule.php
@@ -29,7 +29,8 @@ $sql = "DECLARE @Today nvarchar(20) = DATENAME(dw, GETDATE())";
 odbc_exec($connection, $sql);
 
 // Query the database for the events associated with the user
-$query = "SELECT * FROM UserEvents ue
+$query = "DECLARE @Today nvarchar(20) = DATENAME(dw, GETDATE());
+          SELECT * FROM UserEvents ue
           JOIN Events e ON e.EventId=ue.EventId
           LEFT JOIN ScheduledEventCode sec ON e.EventId = sec.ScheduledEventId
           WHERE UserId= '$userid'
