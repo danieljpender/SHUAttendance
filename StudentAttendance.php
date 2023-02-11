66e5dc690bb71c2d1622f3b074076eb648aa33a2
danieljpender <danielpp4321@hotmail.co.uk>
2023-02-11 22:12:40 +0000

    Update StudentAttendance.php

--- StudentAttendance.php
+++ StudentAttendance.php
@@ -1,9 +1,21 @@
 <?php
-// Start the session
 session_start();
-$user_role = "";
-if (isset($_SESSION['role'])) {
-  $user_role = $_SESSION['role'];
+
+$server = "eam-group27.database.windows.net";
+$database = "SHUAttendance";
+$serverUsername = "eam";
+$serverPassword = "%PA55w0rd";
+
+$connection = odbc_connect("Driver={ODBC Driver 18 for SQL Server};Server=$server;Database=$database;", $serverUsername, $serverPassword);
+
+if (!$connection) {
+    die("Error connecting to database: " . odbc_errormsg());
+}
+
+// Check if the user is logged in
+if (!isset($_SESSION['userid'])) {
+  header("Location: login.php");
+  exit();
 }
 
 $userid = $_SESSION['userid'];
@@ -25,7 +37,63 @@ $role = $_SESSION['role'];
   Student Attendance Records
 </h1>
   <div class="container">
+    <form action="" method="post">
+      <label for="department">Select a Department:</label>
+      <select name="department" id="department" onchange="showModule()">
+        <option value="">Select a Department</option>
+        <?php
+          $query = "SELECT * FROM Department";
+          $result = odbc_exec($connection, $query);
+          while ($row = odbc_fetch_array($result)) {
+            echo '<option value="' . $row['DepartmentID'] . '">' . $row['DepartmentName'] . '</option>';
+          }
+        ?>
+      </select>
+      <div id="module-container" style="display:none">
+        <label for="module">Select a Module:</label>
+        <select name="module" id="module">
+          <option value="">Select a Module</option>
+        </select>
+      </div>
+      <input type="submit" name="submit" value="Submit">
+    </form>
+    <table id="attendance-table" style="display:none">
+      <thead>
+        <tr>
+          <th>User ID</th>
+          <th>Module ID</th>
+          <th>Attendance Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          if (isset($_POST['submit'])) {
+            $department = $_POST['department'];
+            $module = $_POST['module'];
+            $query = "SELECT * FROM UserAttendance WHERE DepartmentID = '$department' AND ModuleID = '$module'";
+            $result = odbc_exec($connection, $query);
+            while ($row = odbc_fetch_array($result)) {
+              echo '<tr>';
+              echo '<td>' . $row['UserID'] . '</td>';
+              echo '<td>' . $row['ModuleID'] . '</td>';
+              echo '<td>' . $row['AttendanceDate'] . '</td>';
+              echo '</tr>';
+            }
+          }
+        ?>
+      </tbody>
+    </table>
   </div>
     </div>
-  </body>
+    <script>
+  function showModule() {
+    var department = document.getElementById("department").value;
+    if (department) {
+      document.getElementById("module-container").style.display = "block";
+    } else {
+      document.getElementById("module-container").style.display = "none";
+    }
+  }
+</script>
+</body>
 </html>
