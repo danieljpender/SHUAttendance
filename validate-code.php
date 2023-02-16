318c1ba529b7a2e43b925f0d9da359be7d0a97d2
danieljpender <danielpp4321@hotmail.co.uk>
2023-02-16 11:52:24 +0000

    Update validate-code.php

--- validate-code.php
+++ validate-code.php
@@ -23,6 +23,8 @@ if (!isset($_SESSION['userid'])) {
 $timetableid = $_POST['timetableid'];
 $code = $_POST['enteredcode'];
 
+var_dump($_POST);
+
 // Query the database for the timetable with the specified timetableid and code
 $query = "SELECT * FROM Timetable WHERE TimetableId='$timetableid' AND [code]=$code";
 $result = odbc_exec($connection, $query);
