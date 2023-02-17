23f7c512c1ef15f14fd39d610a2b0511adab380e
danieljpender <danielpp4321@hotmail.co.uk>
2023-02-17 20:34:42 +0000

    Update MySchedule.php

--- MySchedule.php
+++ MySchedule.php
@@ -268,6 +268,10 @@ $(document).ready(function() {
         }
       });
     });
+     // Add an event listener to the modal close button
+  $('.close').click(function() {
+    $('#code-modal').css('display', 'none');
+  });
   });
 </script>
 
