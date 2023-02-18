01921fe86a41fd97049b1c8c007143d05001d825
danieljpender <danielpp4321@hotmail.co.uk>
2023-02-18 01:09:29 +0000

    Update MySchedule.php

--- MySchedule.php
+++ MySchedule.php
@@ -100,10 +100,10 @@ while ($row = odbc_fetch_array($result)) {
   } else if ($role == 'Student') {
       if ($attendance_recorded) {
         echo "<td><button disabled='disabled'>Attendance Recorded</button></td>";
-      } else if ($event_has_ended) {
-        echo "<td><button disabled='disabled'>Event Ended</button></td>";
       } else if ($no_register) {
         echo "<td><button disabled='disabled'>Register Not Taken</button></td>";
+      } else if ($event_has_ended) {
+        echo "<td><button disabled='disabled'>Event Ended</button></td>";
       } else {
         echo "<td><button class='enter-code-btn' data-timetableid='$timetableid'>Enter Code</button></td>";
       }
