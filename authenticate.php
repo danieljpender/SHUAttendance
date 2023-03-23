269559545f6a0f027d12abf467f6ce21a36ea443
danieljpender <danielpp4321@hotmail.co.uk>
2023-03-23 17:38:00 +0000

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -4,7 +4,7 @@ session_start();
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$server = "eam-group27.c0zwiexiop2w.eu-west-2.rds.amazonaws.com";
+$server = "eam-group27.c0zwiexiop2w.eu-west-2.rds.amazonaws.com,1433";
 $database = "SHUAttendance";
 $serverUsername = "eam";
 $serverPassword = "%PA55w0rd";
