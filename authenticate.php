f60a4724eb972e4ca618d150765ee1784b637ded
danieljpender <danielpp4321@hotmail.co.uk>
2023-03-23 19:23:38 +0000

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -4,39 +4,39 @@ session_start();
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$server = "eam-group27.c0zwiexiop2w.eu-west-2.rds.amazonaws.com,1433";
-$database = "SHUAttendance";
-$serverUsername = "eam";
-$serverPassword = "%PA55w0rd";
-
-$connection = odbc_connect("Driver={ODBC Driver 18 for SQL Server};Server=$server;Database=$database;TrustServerCertificate=yes;", $serverUsername, $serverPassword);
-
-if (!$connection) {
-    die("Error connecting to database: " . odbc_errormsg());
-}
-
-// $serverName = "eam-group27.c0zwiexiop2w.eu-west-2.rds.amazonaws.com,1433";
+// $server = "eam-group27.c0zwiexiop2w.eu-west-2.rds.amazonaws.com,1433";
 // $database = "SHUAttendance";
-// $username = "eam";
-// $password = "%PA55w0rd";
+// $serverUsername = "eam";
+// $serverPassword = "%PA55w0rd";
 
-// $connOptions = array(
-//     "Database" => $database,
-//     "UID" => $username,
-//     "PWD" => $password,
-//     "MultipleActiveResultSets" => false,
-//     "Encrypt" => true,
-//     "TrustServerCertificate" => false,
-//     "LoginTimeout" => 30
-// );
+// $connection = odbc_connect("Driver={ODBC Driver 18 for SQL Server};Server=$server;Database=$database;TrustServerCertificate=yes;", $serverUsername, $serverPassword);
 
-// $conn = sqlsrv_connect($serverName, $connOptions);
-
-// if ($conn === false) {
-//     die(print_r(sqlsrv_errors(), true));
-// } else {
-//     echo "Connected successfully!";
+// if (!$connection) {
+//     die("Error connecting to database: " . odbc_errormsg());
 // }
+
+$serverName = "eam-group27.c0zwiexiop2w.eu-west-2.rds.amazonaws.com,1433";
+$database = "SHUAttendance";
+$username = "eam";
+$password = "%PA55w0rd";
+
+$connOptions = array(
+    "Database" => $database,
+    "UID" => $username,
+    "PWD" => $password,
+    "MultipleActiveResultSets" => false,
+    "Encrypt" => true,
+    "TrustServerCertificate" => true,
+    "LoginTimeout" => 30
+);
+
+$conn = sqlsrv_connect($serverName, $connOptions);
+
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true));
+} else {
+    echo "Connected successfully!";
+}
 
 $sql = "SELECT [userid], [firstname], [surname], u.RoleId, r.rolename as rolename FROM [users] u
         JOIN [Role] r ON r.RoleId = u.RoleId
