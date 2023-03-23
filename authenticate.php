06b27b224a23db1a7ba145e3389a24a368936e42
danieljpender <danielpp4321@hotmail.co.uk>
2023-03-23 17:44:58 +0000

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -4,15 +4,23 @@ session_start();
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$server = "eam-group27.c0zwiexiop2w.eu-west-2.rds.amazonaws.com,1433";
-$database = "SHUAttendance";
-$serverUsername = "eam";
-$serverPassword = "%PA55w0rd";
+// $server = "eam-group27.c0zwiexiop2w.eu-west-2.rds.amazonaws.com,1433";
+// $database = "SHUAttendance";
+// $serverUsername = "eam";
+// $serverPassword = "%PA55w0rd";
 
-$connection = odbc_connect("Driver={ODBC Driver 18 for SQL Server};Server=$server;Database=$database;", $serverUsername, $serverPassword);
+// $connection = odbc_connect("Driver={ODBC Driver 18 for SQL Server};Server=$server;Database=$database;", $serverUsername, $serverPassword);
 
-if (!$connection) {
-    die("Error connecting to database: " . odbc_errormsg());
+// if (!$connection) {
+//     die("Error connecting to database: " . odbc_errormsg());
+// }
+$serverName = "tcp:eam-group27.c0zwiexiop2w.eu-west-2.rds.amazonaws.com,1433";
+$connectionOptions = array("Database"=>"SHUAttendance",
+                           "Uid"=>"eam", "PWD"=>"%PA55w0rd");
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if($conn === false)
+{
+    die(print_r(sqlsrv_errors(), true));
 }
 
 $sql = "SELECT [userid], [firstname], [surname], u.RoleId, r.rolename as rolename FROM [users] u
