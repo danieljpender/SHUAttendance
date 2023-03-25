bc0fd6925c26d40e48b654092bb492bb6a63e1db
danieljpender <danielpp4321@hotmail.co.uk>
2023-03-25 22:36:26 +0000

    Update validate-code.php

--- validate-code.php
+++ validate-code.php
@@ -20,17 +20,49 @@ $connOptions = array(
 $connection = sqlsrv_connect($serverName, $connOptions);
 
 if ($connection === false) {
-    die(print_r(sqlsrv_errors(), true));
+  echo "Unable to connect to database. Error message: ";
+  die(print_r(sqlsrv_errors(), true));
 }
 
 if (isset($_POST['timetableid']) && isset($_POST['code'])) {
   $timetableid = $_POST['timetableid'];
   $code = $_POST['code'];
+  echo $timetableid;
+  echo $code;
 
   // Query the database to check if the code is valid
   $query = "SELECT TimetableId, [code] FROM Timetable WHERE TimetableId = '$timetableid' AND [code] = $code";
   echo $query;
   $result = sqlsrv_query($connection, $query);
+  
+  if ($result === false) {
+    // Print the last SQL error message, if any
+    echo "Unable to execute query. Error message: ";
+    die(print_r(sqlsrv_errors(), true));
+  }
+  if (sqlsrv_has_rows($result)) { // Changed to sqlsrv_has_rows to be more reliable
+    // Code is valid, update the attendance record for the user and the event
+    $userid = $_SESSION['userid'];
+    $query = "INSERT INTO UserAttendanceHistory (UserAttendanceHistoryId, UserId, TimetableId, DateCreated) VALUES (NEWID(), '$userid', '$timetableid', GETUTCDATE())";
+    $result = sqlsrv_query($connection, $query);
+    
+    if ($result === false) {
+      // Print the last SQL error message, if any
+      echo "Unable to execute query. Error message: ";
+      die(print_r(sqlsrv_errors(), true));
+    }
+    
+    echo 'success';
+  } else {
+    // Code is invalid
+    echo 'failure';
+  }
+}
+
+// Close the database connection
+sqlsrv_close($connection);
+?> 
+<!-- 
   if (sqlsrv_has_rows($result)) {
     // Code is valid, update the attendance record for the user and the event
     $userid = $_SESSION['userid'];
@@ -41,4 +73,4 @@ if (isset($_POST['timetableid']) && isset($_POST['code'])) {
     // Code is invalid
     echo 'failure';
   }
-}
+} -->
