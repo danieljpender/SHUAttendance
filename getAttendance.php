69cb2f2d4529ff817787dfe289c643263230d9fe
danieljpender <danielpp4321@hotmail.co.uk>
2023-03-26 23:43:24 +0100

    Update getAttendance.php

--- getAttendance.php
+++ getAttendance.php
@@ -21,39 +21,36 @@ $connection = sqlsrv_connect($serverName, $connOptions);
 if ($connection === false) {
     die(print_r(sqlsrv_errors(), true));
 }
-// Retrieve the department and module values from the URL parameters
-$department = $_GET['department'];
-$module = $_GET['module'];
 
-// Make a database query to retrieve the attendance records
-$query = "SELECT *, u.FullName as student_name FROM UserTimetable ut
-                      --LEFT JOIN UserAttendanceHistory uah ON ut.UserTimetableId = uah.UserTimetableId
-                      JOIN Timetable t ON t.ModuleId = ut.ModuleId
-                      JOIN Users u ON u.UserId = ut.UserId
-                      WHERE ut.DepartmentId = '$department' AND ut.ModuleId = '$module'
-                      AND u.RoleId = '17b1cdac-93f8-4a5f-a5cd-907272094140'";
-            $result = sqlsrv_query($connection, $query);
-            while ($row = sqlsrv_fetch_array($result)) {
-                echo '<tr>';
-                echo '<td>' . $row['student_name'] . '</td>';
-                echo '<td>' . $row['startdate'] . '</td>';
-                echo '<td>' . $row['attendance'] . '</td>';
-                echo '</tr>';
-              }
+if (isset($_GET['department']) && isset($_GET['module'])) {
+  $department = $_GET['department'];
+  $module = $_GET['module'];
+  
+  $query = "SELECT *, u.FullName as student_name, t.StartDate as startdate, u.UserId as userid
+            FROM UserTimetable ut
+            JOIN [User] u ON ut.StudentId = u.UserId
+            JOIN [Module] m ON ut.ModuleId = m.ModuleId
+            WHERE m.DepartmentId = '$department' AND m.ModuleId = '$module'";
+  $result = sqlsrv_query($connection, $query);
+  if ($result === false) {
+    die(print_r(sqlsrv_errors(), true));
+  }
+  
+  while ($row = sqlsrv_fetch_array($result)) {
+    echo '<tr>';
+    echo '<td>' . $row['student_name'] . '</td>';
+    echo '<td>' . $row['startdate']->format('Y-m-d') . '</td>';
+    echo '<td>';
+    if ($row['Attendance'] === null) {
+      echo 'No record';
+    } else {
+      echo $row['Attendance'] == 1 ? 'Present' : 'Absent';
+    }
+    echo '</td>';
+    echo '</tr>';
+  }
+}
 
-// Generate the HTML for the attendance table
-$tableHtml = '<thead>
-  <tr>
-    <th>Student Name</th>
-    <th>Date</th>
-    <th>Attendance Record</th>
-  </tr>
-</thead>
-<tbody>';
-// Loop through the query results and append each row to $tableHtml
-// ...
-$tableHtml .= '</tbody>';
-
-// Return the HTML as the response to the AJAX request
-echo $tableHtml;
+sqlsrv_free_stmt($result);
+sqlsrv_close($connection);
 ?>
