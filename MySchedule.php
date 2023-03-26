bf83d3b4d8f87583d3c74ddeb656f35d672bcfd3
danieljpender <danielpp4321@hotmail.co.uk>
2023-03-26 03:00:44 +0100

    Update MySchedule.php

--- MySchedule.php
+++ MySchedule.php
@@ -83,15 +83,14 @@ $result = sqlsrv_query($connection, $query);
 while ($row = sqlsrv_fetch_array($result)) {
   $timetableid = $row['timetable_id'];
   $startTime = $row['StartTime']->getTimestamp();
-
-$endTime = date("H:i", strtotime($row['EndTime']->format('Y-m-d H:i:s')));
-$end_datetime = date("H:i", strtotime($row['EndDate']->format('Y-m-d H:i:s')));
-  $current_datetime = date("H:i", strtotime('now'));
+$endTime = $row['EndTime']->getTimestamp();
+$end_datetime = $row['EndDate']->getTimestamp();
+  $current_datetime = strtotime('now');
   $event_has_ended = $current_datetime < $end_datetime;
   $code_disabled = $row['timetablecode'] !== NULL;
   $no_register = $row['timetablecode'] == NULL && $current_datetime < ($end_datetime + $endTime);
  //$enter_code_disabled = $role == 'Student' && $now > $event_end_time;
-echo "starttime: " . $startTime ;
+//echo "starttime: " . $startTime ;
 // echo "endtime: " . $startTime;
 // echo "enddate: " . $startTime;
 // echo "current_datedtime: " . $startTime;
