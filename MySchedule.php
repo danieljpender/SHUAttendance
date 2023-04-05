8dd428d186b9beb7a2ad1188c2096d1ebe5426de
danieljpender <danielpp4321@hotmail.co.uk>
2023-04-06 00:28:19 +0100

    Update MySchedule.php

--- MySchedule.php
+++ MySchedule.php
@@ -147,7 +147,7 @@ $timestamp = $lectureEndDate->getTimestamp();
 </div>
 <?php include 'footer.php'; ?>
 </div>
-<div id="event-modal" class="modal-content">
+<div id="code-modal" class="modal-content">
 <form id="code-form" method="POST">  
 <header class="modal-header">
     <h5 class="modal-title">Record Attendance</h5><!---->
